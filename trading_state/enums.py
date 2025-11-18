609ea1594154231e1b7e36c2451532dea7703945
Kael Zhang <i+github@kael.me>
2025-11-18 16:11:08 +0800

    enums

---
+++ trading_state/enums.py
@@ -0,0 +1,99 @@
+from enum import Enum
+
+
+class StringEnum(Enum):
+    def __str__(self):
+        return self.value
+
+
+class OrderType(StringEnum):
+    # Market order
+    MARKET = 'MARKET'
+
+    # Price order
+    LIMIT = 'LIMIT'
+
+    # Stop Loss Orders
+    # ----------------------------------------------
+
+    # Stop market order
+    STOP_LOSS = 'STOP_MARKET'
+
+    # Stop limit order
+    STOP_LOSS_LIMIT = 'STOP_LOSS_LIMIT'
+
+    # Take Profit Orders
+    # ----------------------------------------------
+
+    # Take profit market order
+    TAKE_PROFIT = 'TAKE_PROFIT'
+
+    # Take profit limit order
+    TAKE_PROFIT_LIMIT = 'TAKE_PROFIT_LIMIT'
+
+    # Trailing stop is actualy not an order type,
+    # but an extra settings about when to trigger the stop order,
+    # which could apply to both stop loss orders and take profit orders
+
+
+class MarketQuantityType(StringEnum):
+    BASE = 'BASE'
+    QUOTE = 'QUOTE'
+
+
+class OrderSide(StringEnum):
+    # USDT is locked
+    BUY = 'BUY'
+
+    # BTC is locked
+    SELL = 'SELL'
+
+
+class OrderStatus(Enum):
+    """
+    The status of an order
+    """
+
+    def __str__(self):
+        return self.value[0]
+
+    # The ticket is initialized but has not been submitted to the exchange,
+    # or the ticket is failed to create order so back to the initial state
+    INIT = ('INIT', 0)
+
+    # The order is creating,
+    #   the request is about to send to the exchange,
+    #   but not yet get response from the exchange
+    SUBMITTING = ('SUBMITTING', 1)
+
+    # The order is created via the exchange API
+    CREATED = ('CREATED', 2)
+
+    def lt(self, status: 'OrderStatus') -> bool:
+        """
+        Returns `True` if the current status is less than the given status
+        """
+
+        return self.value[1] <= status.value[1]
+
+
+class TimeInForce(StringEnum):
+    # Good Til Canceled
+    # An order will be on the book unless the order is canceled.
+    GTC = 'GTC'
+
+    # Immediate Or Cancel
+    # An order will try to fill the order as much as it can before the order expires.
+    IOC = 'IOC'
+
+    # Fill or Kill
+    # An order will expire if the full order cannot be filled upon execution.
+    FOK = 'FOK'
+
+
+class STPMode(StringEnum):
+    EXPIRE_MAKER = 'EXPIRE_MAKER'
+    EXPIRE_TAKER = 'EXPIRE_TAKER'
+    EXPIRE_BOTH = 'EXPIRE_BOTH'
+    INCREMENT = 'INCREMENT'
+
