dc2dac548124da778918d9f887e760915cbb03e7
Kael Zhang <i+github@kael.me>
2025-11-18 16:12:04 +0800

    order: order tickets

--- trading_state/order.py
+++ trading_state/order.py
@@ -3,14 +3,14 @@
 
 # from dataclasses import dataclass
 from typing import (
-    List, Union
+    List, Union, Optional
 )
 
 from decimal import Decimal
 
 from .symbol import Symbol
 from .enums import (
-    OrderSide, OrderType, TimeInForce, MarketQuantityType
+    OrderSide, OrderType, TimeInForce, MarketQuantityType, STPMode
 )
 from .util import dynamic_dataclass
 
@@ -29,37 +29,45 @@ class BaseOrderTicket:
     ]
     ADDITIONAL_OPTIONAL_PARAMS: List[str] = []
 
+    @property
+    def REQUIRED_PARAMS(self) -> List[str]:
+        return (
+            self.BASE_MANDOTORY_PARAMS + self.ADDITIONAL_MANDOTORY_PARAMS
+        )
+
+    @property
+    def OPTIONAL_PARAMS(self) -> List[str]:
+        return (
+            self.BASE_OPTIONAL_PARAMS + self.ADDITIONAL_OPTIONAL_PARAMS
+        )
+
+    @property
+    def PARAMS(self) -> List[str]:
+        return (
+            self.REQUIRED_PARAMS + self.OPTIONAL_PARAMS
+        )
+
     symbol: Symbol
     side: OrderSide
     type: OrderType
-    stp: str
+    stp: Optional[STPMode] = None
     others: dict[str, any]
 
     def __init__(
         self,
         **kwargs
     ) -> None:
-        required_params = (
-            self.BASE_MANDOTORY_PARAMS + self.ADDITIONAL_MANDOTORY_PARAMS
-        )
-        optional_params = (
-            self.BASE_OPTIONAL_PARAMS + self.ADDITIONAL_OPTIONAL_PARAMS
-        )
-
-        for param in required_params:
+        for param in self.REQUIRED_PARAMS:
             if param not in kwargs:
                 raise ValueError(f'"{param}" is a required parameter for Order')
 
             setattr(self, param, kwargs[param])
             del kwargs[param]
 
-        for param in optional_params:
+        for param in self.OPTIONAL_PARAMS:
             if param in kwargs:
                 setattr(self, param, kwargs[param])
                 del kwargs[param]
-            else:
-                # Make sure the parameter is set
-                setattr(self, param, None)
 
         self.others = kwargs
         self._validate_params()
@@ -77,9 +85,17 @@ class LimitOrderTicket(BaseOrderTicket):
     quantity: Decimal
     time_in_force: TimeInForce
 
-    ADDITIONAL_OPTIONAL_PARAMS = ['post_only']
+    ADDITIONAL_OPTIONAL_PARAMS = ['post_only', 'iceberg_quantity']
 
-    post_only: bool
+    post_only: bool = False
+    iceberg_quantity: Optional[Decimal] = None
+
+    def _validate_params(self) -> None:
+        if (
+            self.time_in_force is not None
+            and self.post_only
+        ):
+            raise ValueError('post_only is not allowed with time_in_force')
 
 
 class MarketOrderTicket(BaseOrderTicket):
@@ -91,13 +107,19 @@ class MarketOrderTicket(BaseOrderTicket):
     quantity_type: MarketQuantityType
 
 
-def stop_price_or_trailing_delta(self) -> None:
+def validate_stop_price_and_trailing_delta(self) -> None:
     if self.stop_price is None and self.trailing_delta is None:
         raise ValueError('Either stop_price or trailing_delta must be set')
 
     if self.stop_price is not None and self.trailing_delta is not None:
         raise ValueError('Only one of stop_price or trailing_delta can be set')
 
+
+PARAMS_STOP_PRICE_AND_TRAILING_DELTA = ['stop_price', 'trailing_delta']
+PARAMS_ST_AND_ICEBERG_QUANTITY = [
+    *PARAMS_STOP_PRICE_AND_TRAILING_DELTA,
+    'iceberg_quantity'
+]
 
 class StopLossOrderTicket(BaseOrderTicket):
     type = OrderType.STOP_LOSS
@@ -106,12 +128,13 @@ class StopLossOrderTicket(BaseOrderTicket):
 
     quantity: Decimal
 
-    ADDITIONAL_OPTIONAL_PARAMS = ['stop_price','trailing_delta']
+    ADDITIONAL_OPTIONAL_PARAMS = PARAMS_STOP_PRICE_AND_TRAILING_DELTA
 
-    stop_price: Decimal
-    trailing_delta: Decimal
+    stop_price: Optional[Decimal] = None
+    trailing_delta: Optional[Decimal] = None
+    iceberg_quantity: Optional[Decimal] = None
 
-    _validate_params = stop_price_or_trailing_delta
+    _validate_params = validate_stop_price_and_trailing_delta
 
 
 
@@ -123,6 +146,10 @@ class StopLossLimitOrderTicket(StopLossOrderTicket):
     price: Decimal
     time_in_force: TimeInForce
 
+    ADDITIONAL_OPTIONAL_PARAMS = PARAMS_ST_AND_ICEBERG_QUANTITY
+
+    iceberg_quantity: Optional[Decimal] = None
+
 
 class TakeProfitOrderTicket(BaseOrderTicket):
     type = OrderType.TAKE_PROFIT
@@ -133,10 +160,10 @@ class TakeProfitOrderTicket(BaseOrderTicket):
 
     ADDITIONAL_OPTIONAL_PARAMS = ['stop_price', 'trailing_delta']
 
-    stop_price: Decimal
-    trailing_delta: Decimal
+    stop_price: Optional[Decimal] = None
+    trailing_delta: Optional[Decimal] = None
 
-    _validate_params = stop_price_or_trailing_delta
+    _validate_params = validate_stop_price_and_trailing_delta
 
 
 class TakeProfitLimitOrderTicket(TakeProfitOrderTicket):
@@ -147,8 +174,11 @@ class TakeProfitLimitOrderTicket(TakeProfitOrderTicket):
     price: Decimal
     time_in_force: TimeInForce
 
+    ADDITIONAL_OPTIONAL_PARAMS = PARAMS_ST_AND_ICEBERG_QUANTITY
+    iceberg_quantity: Optional[Decimal] = None
 
-Order = Union[
+
+OrderTicket = Union[
     LimitOrderTicket,
     MarketOrderTicket,
     StopLossOrderTicket,
