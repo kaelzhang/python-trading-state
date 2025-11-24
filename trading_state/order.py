ec4fadbd1ce04187ea667067ac5de66058f27529
Kael Zhang <i+github@kael.me>
2025-11-24 22:00:08 +0800

    order: complete Order, add OrderHistory

--- trading_state/order.py
+++ trading_state/order.py
@@ -1,95 +1,147 @@
 from decimal import Decimal
-from typing import Set, Callable, Awaitable
+from typing import (
+    Callable, Awaitable, Optional,
+    List, Iterator,
+)
 
-from .types import OrderStatus, OrderSide, OrderTicket, SymbolPosition
-from .util import class_repr
+from .enums import (
+    OrderStatus
+)
+from .order_ticket import (
+    OrderTicket
+)
+from .types import (
+    SymbolPosition,
+)
+from .common import (
+    class_repr,
+    DECIMAL_ZERO,
+)
+
+
+StatusUpdatedCallback = Callable[[OrderStatus], None]
 
 
 class Order:
-    __slots__ = (
-        'ticket',
-        'status'
-    )
-
     _UID: int = 0
 
+    # Immutable properties
+    id: int
     ticket: OrderTicket
-    status: OrderStatus
+    position: SymbolPosition
     locked_asset: str
     locked_quantity: Decimal
+
+    # Mutable properties
+    _status: OrderStatus
+    filled_quantity: Decimal
+
+    _status_updated_callback: Optional[StatusUpdatedCallback]
+
+    def __repr__(self) -> str:
+        return class_repr(self, keys=[
+            'id',
+            'ticket',
+            'status',
+            'position',
+        ])
 
     def __init__(
         self,
         ticket: OrderTicket,
+        position: SymbolPosition,
 
         # The quantity of which asset has been locked,
         # could be either base asset or quote asset
         locked_asset: str,
         # locked quantity
-        locked_quantity: Decimal,
-
-        position: SymbolPosition
+        locked_quantity: Decimal
     ) -> None:
         self.id = OrderTicket._UID
-
         OrderTicket._UID += 1
 
+        self._status_updated_callback = None
+
         self.ticket = ticket
-        self.status = OrderStatus.INIT
+        self.position = position
         self.locked_asset = locked_asset
         self.locked_quantity = locked_quantity
 
-    __repr__ = class_repr
+        self._status = OrderStatus.INIT
+        self.filled_quantity = DECIMAL_ZERO
+
+    def when_status_updated(
+        self,
+        callback: StatusUpdatedCallback
+    ) -> None:
+        self._status_updated_callback = callback
+
+    def _trigger_status_updated(self, status: OrderStatus) -> None:
+        if self._status_updated_callback is not None:
+            self._status_updated_callback(status)
+
+    @property
+    def status(self) -> OrderStatus:
+        return self._status
+
+    @status.setter
+    def status(self, status: OrderStatus) -> None:
+        self._status = status
+        self._trigger_status_updated(status)
+
+        if (
+            status is OrderStatus.CANCELLED
+            or status is OrderStatus.FILLED
+        ):
+            # Clean the callback to avoid memory leak
+            self._status_updated_callback = None
 
 
 class OrderHistory:
-    ...
+    _history: List[Order]
 
-
-class OrderGroup:
-    """
-    Group sell and group buy are mutually exclusive
-    """
-
-    __slots__ = (
-        'buy',
-        'sell'
-    )
-
-    # The tickets that buy the asset
-    buy: Set[Order]
-    # The tickets that sell the asset
-    sell: Set[Order]
-
-    def __init__(self):
-        self.buy = set()
-        self.sell = set()
-
-    def get(
+    def __init__(
         self,
-        direction: OrderSide
-    ) -> Set[Order]:
+        max_size: int
+    ) -> None:
+        self._max_size = max_size
+        self._history = []
+
+    def __len__(self) -> int:
+        return len(self._history)
+
+    def __iter__(self) -> Iterator[Order]:
+        return iter(self._history)
+
+    def _check_size(self) -> None:
+        if len(self._history) > self._max_size:
+            self._history.pop(0)
+
+    def append(
+        self,
+        order: Order
+    ) -> None:
+        self._history.append(order)
+        self._check_size()
+
+    def query(
+        self,
+        **criteria
+    ) -> List[Order]:
         """
-        Get a copied group of the given direction
+        Query the history orders by the given criteria
         """
 
-        return (
-            self.buy if direction is OrderSide.BUY else self.sell
-        ).copy()
+        matched = []
 
-    def close(self, order: Order) -> None:
-        """
-        Close a ticket
-        """
+        for order in self._history:
+            if all(
+                getattr(order, key) == value
+                for key, value in criteria.items()
+            ):
+                matched.append(order)
 
-        (self.buy or self.sell).discard(order)
-
-    def clear(self) -> None:
-        """
-        Clear all tickets
-        """
-
-        (self.buy or self.sell).clear()
+        return matched
 
 
 FuncCancelOrder = Callable[[Order], Awaitable[None]]
